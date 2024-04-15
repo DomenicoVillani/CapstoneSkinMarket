8e425032d1698ad7f247db4326143414efaabb94
Domenico Villani <domenico.villani.99@gmail.com>
2024-04-15 18:19:42 +0200

    invio email pagina contattaci

--- Controllers/FooterController.cs
+++ Controllers/FooterController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using CapstoneSkinMarket.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,6 +31,48 @@ namespace CapstoneSkinMarket.Controllers
         public ActionResult Contattaci()
         {
             return View();
+        }
+        public ActionResult EmailMandata()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SendEmail(EmailElastic email)
+        {
+            try
+            {
+                string smtpServer = "smtp.elasticemail.com";
+                int smtpPort = 2525;
+                string smtpUser = "skinbazaarclient@gmail.com";
+                string smtpPassword = "5F412D24268B10746B6918CC1CCEAB5BDF58";
+
+
+                using (var smtpClient = new SmtpClient(smtpServer, smtpPort))
+                {
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Credentials = new NetworkCredential(smtpUser, smtpPassword);
+                    smtpClient.EnableSsl = true;
+
+
+                    var mailMessage = new MailMessage
+                    {
+                        From = new MailAddress(smtpUser),
+                        Subject = email.Oggetto,
+                        Body = $"Email mandata da "+ email.EmailMittente +"\n"+ email.ContenutoEmail,
+                        IsBodyHtml = false,
+                    };
+
+                    mailMessage.To.Add(smtpUser);
+
+                    await smtpClient.SendMailAsync(mailMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = "Errore: " + ex.Message;
+            }
+            return View("EmailMandata");
         }
     }
 }
---
+++ Models/EmailElastic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneSkinMarket.Models
+{
+    public class EmailElastic
+    {
+        public string NomeCognome { get; set; }
+        public string Oggetto { get; set; }
+        public string ContenutoEmail { get; set; }
+        public string EmailMittente { get; set; }
+
+    }
+}
